1cbc42ce0ccc5342c753e1cbf611ef9b88bbf0ec
Macca <maccalukic@icloud.com>
2023-05-02 13:57:06 +0800

    Javascript progress and styling addtions

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@
 
 const formEl = document.getElementById('search-form');
 const searchEl = document.getElementById('search');
-
 let searchFormEl = document.querySelector('#search-form')
 
 
@@ -11,69 +10,73 @@ let searchFormEl = document.querySelector('#search-form')
 
 function searchApi() {
 
-  
+
   const searchInput = document.getElementById('searchEntry');
+
 
   let queryString = `http://api.openweathermap.org/data/2.5/forecast?q=${searchInput.value}&appid=d41a700a8c02c2dcea0a697c4dbe482d&units=metric`
   console.log(searchInput.value);
 
   fetch(queryString)
-      .then(response => response.json())
-      .then(data => {
-          console.log(data)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
 
-          const parentDiv = document.getElementById('output');
-          parentDiv.innerHTML = '';
-
-  
-
-          const weatherData = data.list[0].main;
-          console.log(weatherData)
+      const parentDiv = document.getElementById('output');
+      parentDiv.innerHTML = '';
 
 
-          const weatherOutput = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-              weatherOutput.push(data.list[i].main.temp);
-              weatherOutput.push(data.list[i].dt_txt);
-              weatherOutput.push(data.list[i].icon);
-              weatherOutput.push(data.list[i].main.humidity);
-              weatherOutput.push(data.list[i].wind.speed);
-          }
 
-          // weatherOutput.forEach(temp => console.log(temp));
-          
-          const mainTemp = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-            mainTemp.push(data.list[i].main.temp);
-          }
-          
-          const dtTxt = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-            dtTxt.push(data.list[i].dt_txt);
-          }
-          
-          const weatherIcon = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-            weatherIcon.push(data.list[i].weather.icon);
-          }
-          
-          const mainHumidity = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-            mainHumidity.push(data.list[i].main.humidity);
-          }
-          
-          const wind = [];
-          for (let i = 5; i < data.list.length; i += 6) {
-            wind.push(data.list[i].wind.speed);
-          }
-          
+      const weatherData = data.list[0].main;
+      console.log(weatherData)
 
-          
-          for (let i = 0; i < mainTemp.length; i++) {
-            const childDiv = document.createElement('div');
-            childDiv.style.display = 'inline-block';
-            childDiv.style.border = '1px solid black';
-            childDiv.innerHTML = `
+      console.log(data.list[0].weather[0].icon)
+      const icon = `https://openweathermap.org/img/w/` + data.list[0].weather[0].icon + `.png`
+
+      const output = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        output.push(data.list[i].main.temp);
+        // output.push(data.list[i].weather[i].icon);
+        output.push(data.list[i].main.humidity);
+        output.push(data.list[i].wind.speed);
+      }
+
+      
+
+      // weatherOutput.forEach(temp => console.log(temp));
+
+      const mainTemp = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        mainTemp.push(data.list[i].main.temp);
+      }
+
+      const dtTxt = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        dtTxt.push(data.list[i].dt_txt.slice(0,10));
+      }
+
+      const weatherIcon = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        weatherIcon.push(`https://openweathermap.org/img/w/` + data.list[0].weather[0].icon + `.png`);
+      }
+
+      const mainHumidity = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        mainHumidity.push(data.list[i].main.humidity);
+      }
+
+      const wind = [];
+      for (let i = 5; i < data.list.length; i += 6) {
+        wind.push(data.list[i].wind.speed);
+      }
+
+
+
+      for (let i = 0; i < mainTemp.length; i++) {
+        const childDiv = document.createElement('div');
+        childDiv.style.display = 'inline-block';
+        childDiv.style.border = '1px solid black';
+        childDiv.innerHTML = `
               <p>Temp: ${mainTemp[i]}</p>
               <p> ${dtTxt[i]}</p>
               <img src="${weatherIcon[i]}">
@@ -81,37 +84,51 @@ function searchApi() {
               <p>Wind: ${wind[i]}</p>
             `;
 
-             // Add big-div class to the first child div
-             if (i === 0) {
-              childDiv.classList.add('big-div');
-            } else if (i != 0) {
-              childDiv.classList.add('small-div');
-            }
+        // Add big-div class to the first child div
+        if (i === 0) {
+          childDiv.classList.add('big-div');
+        } else if (i != 0) {
+          childDiv.classList.add('small-div');
+        }
 
-            parentDiv.appendChild(childDiv);
-
-          }
-
-          
+        parentDiv.appendChild(childDiv);
 
 
-      })
 
-      .catch(error => console.error(error));
+      }
+      // Save search entry.
 
-  
+      localStorage.setItem('searchEntry', searchInput.value);
+      // using local storage to display a history of past searches
+
+      const storedData = localStorage.getItem('searchEntry'); // Replace 'myData' with the name of your local storage item
+      if (storedData) {
+        const button = document.createElement('button');
+        button.textContent = storedData;
+        const localDiv = document.getElementById('local-storage');
+        localDiv.appendChild(button);
+      }
+
+    });
 }
 
-  
+      // .catch(error => console.error(error));
+
+
+    
+
+// Local storage to dispaly history of past searches
+
+
+
+
+
 // Handle submit button
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
-    
-    searchApi();
-    
-}
+        event.preventDefault();
+        searchApi();
+      }
+
 
   searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-
